aecf98823a7d31e1b0b6f0192c954338b364f679
jaeduk <woejr2332@gamil.com>
2023-03-27 15:29:36 +0900

    최재덕/2023.03.27/ queryDSL 설정

--- src/main/java/swcampus/mvc/repository/ReviewRepository.java
+++ src/main/java/swcampus/mvc/repository/ReviewRepository.java
@@ -1,10 +1,12 @@
 package swcampus.mvc.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import swcampus.mvc.domain.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, QuerydslPredicateExecutor<Review>{
-
+	
+	Page<Review> findByLectureLectureNo(Long lectureNo);
 }
--- src/test/java/swcampus/mvc/choi/ReviewTest.java
+++ src/test/java/swcampus/mvc/choi/ReviewTest.java
@@ -5,9 +5,12 @@ import javax.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.test.annotation.Commit;
 
+import swcampus.mvc.domain.Review;
 import swcampus.mvc.dto.review.ReviewRequestDTO;
+import swcampus.mvc.repository.ReviewRepository;
 import swcampus.mvc.service.ReviewService;
 
 @SpringBootTest
@@ -17,17 +20,33 @@ public class ReviewTest {
 	
 	@Autowired
 	private ReviewService reviewService;
+	
+	@Autowired
+	private ReviewRepository reviewRep;
+	
 	@Test
 	void reviewInsert() {
 		ReviewRequestDTO reviewDTO = 
 				ReviewRequestDTO.builder()
 				.lectureNo(1L)
-				.userNo(3L)
-				.reviewStar(4.5)
+				.userNo(2L)
+				.reviewStar(5)
 				.reviewContent("이 강의를 들으면 바로 취직 가능합니다")
-				.reviewImg("aa.img")
+				.reviewImg("kosta.img")
 				.build();
 		
 		reviewService.reviewInsert(reviewDTO);
 	}
+	
+	
+	@Test
+	void reviewSelect() {
+		Page<Review> list = reviewRep.findByLectureLectureNo(1L);
+		
+		for(Review l:list) {
+			System.out.println(l);
+		}
+		
+	}
+	
 }
