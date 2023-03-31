a0c27aec17175ffe556d9a0d460070cfa11ecea2
jihyeon0624 <wlgus990624@gmail.com>
2023-03-31 17:56:45 +0900

    김지현/2023.03.31/강의상세수정

--- src/main/java/swcampus/mvc/controller/TestController.java
+++ src/main/java/swcampus/mvc/controller/TestController.java
@@ -11,13 +11,17 @@ public class TestController {
 	      System.out.println("HomeController의 index.....");
 	      return "/main";//
 	   }
-/*
- * @RequestMapping("/details/{url}") public void url() {}
- * 
- * @RequestMapping("/board/{url}") public void board() {}
- * 
- * @RequestMapping("/utill/{url}") public void utill() {}
- * 
- * @RequestMapping("/insertForm/{url}") public void insertForm() {} }
- */
+		/*
+		 * @RequestMapping("/details/{url}") public void url() {}
+		 * 
+		 * @RequestMapping("/board/{url}") public void board() {}
+		 * 
+		 * @RequestMapping("/utill/{url}") public void utill() {}
+		 * 
+		 * @RequestMapping("/insertForm/{url}") public void insertForm() {} }
+		 */
+	   @RequestMapping("/utill/{url}") public void url() {}
+	   @RequestMapping("/mypage/{url}") public void mypage() {}
+	   @RequestMapping("/details/{url}") public void details() {}
+	   @RequestMapping("/admin/{url}") public void admin() {}
 }
